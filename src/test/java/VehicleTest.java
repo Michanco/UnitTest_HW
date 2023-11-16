036b11d56ef03bf668ba33ec3090c9433abb201e
Michanco Slesarev <michanco80@gmail.com>
2023-11-16 20:51:55 +0300

    Tests ready

--- src/test/java/VehicleTest.java
+++ src/test/java/VehicleTest.java
@@ -36,10 +36,35 @@ class VehicleTest {
     void motorcycleHaveTwoWheels(){
         assertThat(motorcycle.getNumWheels()).isEqualTo(2);
     }
-
-
-
-
-
-
+    /* Проверить, что объект Car развивает скорость 60 в режиме тестового вождения (используя метод testDrive()).*/
+    @Test
+    void carHaveTestSpeed(){
+        car.testDrive();
+        assertThat(car.getSpeed()).isEqualTo(60);
+    }
+    /*Проверить, что объект Motorcycle развивает скорость 75
+    в режиме тестового вождения (используя метод testDrive()).*/
+    @Test
+    void motorcycleHaveTestSpeed(){
+        motorcycle.testDrive();
+        assertThat(motorcycle.getSpeed()).isEqualTo(75);
+    }
+    /*Проверить, что в режиме парковки (сначала testDrive, потом park,
+    т.е. эмуляция движения транспорта) машина останавливается (speed = 0).
+     */
+    @Test
+    void carMoveTest(){
+        car.testDrive();
+        car.park();
+        assertThat(car.getSpeed()).isEqualTo(0);
+    }
+    /*Проверить, что в режиме парковки (сначала testDrive, потом park,
+    т.е. эмуляция движения транспорта) мотоцикл останавливается (speed = 0).
+     */
+    @Test
+    void motorcycleMoveTest(){
+        motorcycle.testDrive();
+        motorcycle.park();
+        assertThat(motorcycle.getSpeed()).isEqualTo(0);
+    }
 }
